1eec23bc40f180c3d4c985f0d6dd5db45d11a236
asierba <asierba@gmail.com>
2015-09-18 17:57:44 +0100

    Make ActionFactory more readable

--- Barker/Barker/Delivery/CLI/ActionFactory.cs
+++ Barker/Barker/Delivery/CLI/ActionFactory.cs
@@ -9,6 +9,8 @@ namespace Barker.Delivery.CLI
     public class ActionFactory : IActionFactory
     {
         private const string MessageSeparator = "->";
+        private const string ShowWallIdentifier = "wall";
+        private const string FollowIfentifier = "follows";
 
         private readonly IUserRepository _userRepository;
         private readonly IBarksPrinter _barksPrinter;
@@ -23,25 +25,41 @@ namespace Barker.Delivery.CLI
 
         public IAction Create(string input)
         {
-            if (input.Contains(" follows"))
-            {
-                var username = input.Remove(input.LastIndexOf(" follows"));
-                var following = input.Substring(input.LastIndexOf(" follows") + " follows".Length + 1);
-                return new Follow(username, following, _userRepository);
-            }
-            if (input.Contains(" wall"))
-            {
-                var username = input.Remove(input.LastIndexOf(" wall"));
-                return new ShowWall(username, _userRepository, _barksPrinter);
-            }
+            if (input.Contains(" " + FollowIfentifier))
+                return CreateFollow(input);
+            if (input.Contains(" " + ShowWallIdentifier))
+                return CreateShowWall(input);
             if (input.Contains(MessageSeparator))
-            {
-                var inputChunks = input.Split(new[] { MessageSeparator }, StringSplitOptions.None);
-                var username = inputChunks[0].Trim();
-                var messages = inputChunks.Skip(1).Select(x => x.Trim()).ToList();
-                return new Post(username, messages, _userRepository, _clock);
-            }
-            return new ShowBarks(input.Trim(), _userRepository, _barksPrinter);
+                return CreatePost(input);
+            return CreateShowBarks(input);
+        }
+
+        private IAction CreateFollow(string input)
+        {
+            var pattern = " " + FollowIfentifier;
+            var username = input.Remove(input.LastIndexOf(pattern));
+            var followingUser = input.Substring(input.LastIndexOf(pattern) + pattern.Length + 1);
+            return new Follow(username, followingUser, _userRepository);
+        }
+
+        private IAction CreateShowWall(string input)
+        {
+            var username = input.Remove(input.LastIndexOf(" " + ShowWallIdentifier));
+            return new ShowWall(username, _userRepository, _barksPrinter);
+        }
+
+        private IAction CreatePost(string input)
+        {
+            var inputChunks = input.Split(new[] {MessageSeparator}, StringSplitOptions.None);
+            var username = inputChunks[0].Trim();
+            var messages = inputChunks.Skip(1).Select(x => x.Trim()).ToList();
+            return new Post(username, messages, _userRepository, _clock);
+        }
+
+        private IAction CreateShowBarks(string input)
+        {
+            var username = input.Trim();
+            return new ShowBarks(username, _userRepository, _barksPrinter);
         }
     }
 }
